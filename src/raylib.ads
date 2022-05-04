34885b472a8a34573b1375ec820cd5ca4df96b54
Anton Fediushin <imantonmeep@gmail.com>
2022-05-04 12:30:54 +0200

    I don't want to do it manually anymore

--- src/raylib.ads
+++ src/raylib.ads
@@ -165,7 +165,7 @@ package RayLib is
    function Load_VR_Stereo_Config(Device : VR_Device_Info) return VR_Stereo_Config;
    procedure Unload_VR_Stereo_Config(Config : VR_Stereo_Config);
 
-   function Load_Shader(VS_File_Name, FS_File_Name : String) return Shader;
+   function Load_Shader(VS_Name, FS_Name : String) return Shader;
    function Load_Shader_From_Memory  (VS_Code, FS_Code : String) return Shader;
    function Get_Shader_Location(Shader : RayLib.Shader; Uniform_Name : String) return Integer;
    function Get_Shader_Location_Attrib(Shader : RayLib.Shader; Attrib_Name : String) return Integer;
@@ -186,7 +186,7 @@ package RayLib is
 
    function Get_Random_Value(Min, Max : Integer) return Integer;
    procedure Set_Random_Seed(Seed : Natural);
-   procedure Take_Screenshot(File_Name : String);
+   procedure Take_Screenshot(Name : String);
    procedure Set_Config_Flags(Flags : Config_Flag);
 
    --  TODO: Trace_Log
@@ -199,18 +199,18 @@ package RayLib is
    procedure Set_Load_File_Text_Callback(Cb : Load_File_Text_Callback);
    procedure Set_Save_File_Text_Callback(Cb : Save_File_Text_Callback);
 
-   function Load_File_Data(File_Name : String) return Stream_Element_Array;
+   function Load_File_Data(Name : String) return Stream_Element_Array;
    --  no need for unload*
-   function Save_File_Data(File_Name : String; Data : Stream_Element_Array) return Boolean;
+   function Save_File_Data(Name : String; Data : Stream_Element_Array) return Boolean;
    --  TODO: make a procedure?
-   function Load_File_Text(File_Name : String) return String;
-   function Save_File_Text(File_Name : String; Text : String);
-   function File_Exists(File_Name : String) return Boolean;
+   function Load_File_Text(Name : String) return String;
+   function Save_File_Text(Name : String; Text : String);
+   function File_Exists(Name : String) return Boolean;
    function Directory_Exists(Dir_Path : String) return Boolean;
-   function Is_File_Extension(File_Name, Extension : String) return Boolean;
-   function Get_File_Extension(File_Name : String) return String;
-   function Get_File_Name(File_Path : String) return String;
-   function Get_File_Name_Without_Extension(File_Path : String) return String;
+   function Is_File_Extension(Name, Extension : String) return Boolean;
+   function Get_File_Extension(Name : String) return String;
+   function Get_Name(File_Path : String) return String;
+   function Get_Name_Without_Extension(File_Path : String) return String;
    function Get_Directory_Path(File_Path : String) return String;
    function Get_Previous_Directory_Path(Dir_Path : String) return String;
    function Get_Working_Directory return String;
@@ -219,7 +219,7 @@ package RayLib is
    --  TODO: Make a procedure?
    function Is_File_Dropped return Boolean;
    --  TODO: Get_Dropped_Files
-   function Get_File_Modification_Time(File_Name : String) return Time;
+   function Get_File_Modification_Time(Name : String) return Time;
 
    function Compress_Data(Data : Stream_Element_Array) return Stream_Element_Array;
    function Decompress_Data(Data : Stream_Element_Array) return Stream_Element_Array;
@@ -247,7 +247,59 @@ package RayLib is
    procedure Draw_Line(Start_Position_X, Start_Position_Y, End_Position_X, End_Position_T : Integer; Color : RayLib.Color);
    procedure Draw_Line(Start_Position, End_Position : Vector2; Color : RayLib.Color);
    procedure Draw_Line(Start_Position, End_Position : Vector2; Thickness : Float; Color : RayLib.Color);
+   procedure Draw_Line_Bezier(Start_Position, End_Position : Vector2; Thickness : Float; Color : RayLib.Color);
+   procedure Draw_Line_Bezier(Start_Position, End_Position, Control_Position : Vector2; Thickness : Float; Color : RayLib.Color);
+   procedure Draw_Line_Bezier(Start_Position, End_Position, Start_Control_Position, End_Control_Position : Vector2; Thickness : Float; Color : RayLib.Color);
+   --  TODO: Draw_Line_Strip
+   procedure Draw_Circle(Center_X, Center_Y : Integer; Radius : Float; Color : RayLib.Color);
+   procedure Draw_Circle_Sector(Center : Vector2; Radius, Start_Angle, End_Angle : Float; Segments : Integer; Color : RayLib.Color);
+   procedure Draw_Circle_Sector_Lines(Center : Vector2; Radius, Start_Angle, End_Angle : Float; Segments : Integer; Color : RayLib.Color);
+   procedure Draw_Circle_Gradient(Center_X, Center_Y : Integer; Radius : Float; Color_1, Color_2 : RayLib.Color);
+   procedure Draw_Circle(Center : Vector2; Radius : Float; Color : RayLib.Color);
+   procedure Draw_Circle_Lines(Center_X, Center_Y : Integer; Radius : Float; Color : RayLib.Color);
+   procedure Draw_Ellipse(Center_X, Center_Y : Integer; Radius_H, Radius_V : Float; Color : RayLib.Color);
+   procedure Draw_Ellipse_Lines(Center_X, Center_Y : Integer; Radius_H, Radius_V : Float; Color : RayLib.Color);
+   procedure Draw_Ring(Center : Vector2; Inner_Radius, Outer_Radius, Start_Angle, End_Angle : Float; Segments : Integer; Color : RayLib.Color);
+   procedure Draw_Ring_Lines(Center : Vector2; Inner_Radius, Outer_Radius, Start_Angle, End_Angle : Float; Segments : Integer; Color : RayLib.Color);
+   procedure Draw_Rectangle(Position_X, Position_Y, Width, Height : Integer; Color : RayLib.Color);
+   procedure Draw_Rectangle(Position, Size : Vector2; Color : RayLib.Color);
+   procedure Draw_Rectangle(Rectangle : RayLib.Rectangle; Color : RayLib.Color);
+   procedure Draw_Rectangle(Rectangle : RayLib.Rectangle; Origin : Vector2; Rotation : Float; Color : RayLib.Color);
+   procedure Draw_Rectangle_Gradient_Vertical(Position_X, Position_Y, Width, Height : Integer; Color_1, Color_2 : RayLib.Color);
+   procedure Draw_Rectangle_Gradient_Horizontal(Position_X, Position_Y, Width, Height : Integer; Color_1, Color_2 : RayLib.Color);
+   procedure Draw_Rectangle_Gradient(Rectangle : RayLib.Rectangle;  Color_1, Color_2, Color_3, Color_4 : RayLib.Color);
+   procedure Draw_Rectangle_Lines(Position_X, Position_Y, Width, Height : Integer; Color : RayLib.Color);
+   procedure Draw_Rectangle_Lines(Rectangle : RayLib.Rectangle; Line_Thickness : Float; Color : RayLib.Color);
+   procedure Draw_Rectangle_Rounded(Rectangle : RayLib.Rectangle; Roundness : Float; Segments : Integer; Color : RayLib.Color);
+   procedure Draw_Rectangle_Rounded_Lines(Rectangle : RayLib.Rectangle; Roundness : Float; Segments : Integer; Line_Thickness : Float; Color : RayLib.Color);
+   procedure Draw_Triangle(V1, V2, V3 : Vector2; Color : RayLib.Color);
+   procedure Draw_Triangle_Lines(V1, V2, V3 : Vector2; Color : RayLib.Color);
+   --  TODO: Draw_Triangle_Fan, Strip
+   procedure Draw_Polygon(Center : Vector2; Sides : Integer; Radius, Rotation : Float; Color : RayLib.Color);
+   procedure Draw_Polygon_Lines(Center : Vector2; Sides : Integer; Radius, Rotation : Float; Color : RayLib.Color);
+   procedure Draw_Polygon_Lines(Center : Vector2; Sides : Integer; Radius, Rotation, Line_Thickness : Float; Color : RayLib.Color);
+   
 
+   function Check_Collision_Rectangles(Rectangle_1, Rectangle_2 : RayLib.Rectangle) return Boolean;
+   function Check_Collision_Circles(Center_1 : Vector2; Radius_1 : Float; Center_2: Vector2; Radius_2 : Float) return Boolean;
+   function Check_Collision_Circle_Rectangle(Center : Vector2; Radius : Float; Rectangle : RayLib.Rectangle) return Boolean;
+   function Check_Collision_Point_Rectangle(Point : Vector2; Rectangle : RayLib.Rectangle) return Boolean;
+   function Check_Collision_Point_Circle(Point, Center : Vector2; Radius : Float) return Boolean;
+   function Check_Collision_Point_Triangle(Point, P1, P2, P3 : Vector2) return Boolean;
+   function Check_Collision_Lines(Start_Position_1, End_Position_1, Start_Position_2, End_Position_2 : Vector2; Collision_Point : out Vector2) return Boolean;
+   function Check_Collision_Point_Line(Point, P1, P2 : Vector2; Threshold : Integer) return Boolean;
+   function Get_Collision_Rectangle(Rectangle_1, Rectangle_2 : RayLib.Rectangle) return Rectangle;
+
+
+   function Load_Image(Name : String) return Image;
+   function Load_Image(Name : String; Width, Height : Integer; Format : Pixel_Format; Header_Size : Integer) return Image;
+   --  TODO: Load_Image_Anim
+   function Load_Image_From_Memory(File_Type : String; Data : Stream_Element_Array) return Image;
+   function Load_Image_From_Texture(Texture : Texture_2D) return Image;
+   
+   
+   
+   
 
    procedure Draw_Text
      (Text  : String; Position_X, Position_Y : Integer; Font_Size : Integer;
